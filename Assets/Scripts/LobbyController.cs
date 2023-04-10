918ec0d4184c20faa1a7ee32d929a367e2894e3a
Mickman321 <97745994+Mickman321@users.noreply.github.com>
2023-04-11 01:52:50 +0200

    Lobby manager

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -24,8 +24,43 @@ public class LobbyController : MonoBehaviourPunCallbacks
 
     void CreateRoom()
     {
+        print("creating room");
 
+        int randomRoomNumber = Random.Range(0, 100000);
+
+        print(randomRoomNumber);
+
+        RoomOptions options = new RoomOptions()
+        {
+
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = (byte)roomSize
+        };
+
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, options);
     }
+
+    
+
+        public override void OnCreateRoomFailed(short returnCode, string message) 
+        {
+
+         CreateRoom();
+
+        }
+
+        public void Cancel()
+        {
+
+         PhotonNetwork.LeaveRoom();
+        }
+
+            
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
